97a4fab806b28020851dd8278149f6327f6b5662
Wilson Delgado <Wilsondelgadomoreno@gmail.com>
2024-11-21 10:46:32 -0500

    inicializacion de for

---
+++ src/VISTA/Main.java
@@ -0,0 +1,15 @@
+package VISTA;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        //Manejo de Sintaxix del For
+        //  inicializacion condicion incremeneto
+        for (int cont=2;cont<=20;cont++){
+            float nota= JOptionPane.
+          System.out.println("terminamos el eleemento lista");
+        };
+    }
+}
