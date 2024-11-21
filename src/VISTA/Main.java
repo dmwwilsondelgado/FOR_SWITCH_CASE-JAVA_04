fdc9c05b08f78d6f00cb9b402d4e7db6dc886abe
Wilson Delgado <Wilsondelgadomoreno@gmail.com>
2024-11-21 11:17:10 -0500

    Ejercicio de Ejemplo

--- src/VISTA/Main.java
+++ src/VISTA/Main.java
@@ -7,9 +7,10 @@ public class Main {
         System.out.println("Hello, World!");
         //Manejo de Sintaxix del For
         //  inicializacion condicion incremeneto
+
         float promedio =0;
         int cont;
-        for (int cont=1;cont<=20;cont++){
+        for (cont = 1; cont<=20; cont++){
             float nota=Float.parseFloat(JOptionPane.showInputDialog("ingrese la nota"));
             promedio=promedio+nota;// variable acomulativo requiere de un elemento inicial
           System.out.println("la sumatoria de notas"+promedio);
@@ -21,17 +22,16 @@ public class Main {
         //explicacion swicth
         int opc=Integer.parseInt(JOptionPane.showInputDialog("Ingrese la opcion"));
         switch (opc){
-            case 1;
-            for (int cont=1;cont<=5;cont++){
+            case 1:
+            for (cont=1;cont<=5;cont++){
                 System.out.println(cont*10);
             }
-            break //rompa la opcion del case
-            case 2;
+            break ;//rompa la opcion del case
+            case 2:
             System.out.println("ingrese una opcion valida ...");
             break;
             default://opcional
                 System.out.println("opcion no valida");
-
         }
     }
 }
